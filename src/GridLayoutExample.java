bac80d0bebaf17228468e12be41e57cc4fe57446
thchang99 <thchang99@gmail.com>
2024-05-30 03:08:08 +0900

    updating

--- src/GridLayoutExample.java
+++ src/GridLayoutExample.java
@@ -1,43 +1,39 @@
+
 // import statements  
-import java.awt.*;    
-import javax.swing.*;    
-    
-public class GridLayoutExample  
-{    
-JFrame frameObj;    
-  
-// constructor  
-GridLayoutExample()  
-{    
-frameObj = new JFrame();    
-  
-// creating 9 buttons  
-ImageIcon img = new ImageIcon("./images/mic.png");
+import java.awt.*;
+import javax.swing.*;
 
+public class GridLayoutExample {
+    JFrame frameObj;
 
-JButton btn1 = new JButton(img);    
-JButton btn2 = new JButton(img);    
-JButton btn3 = new JButton(img);    
+    // constructor
+    GridLayoutExample() {
+        frameObj = new JFrame();
 
-    
-// adding buttons to the frame  
-// since, we are using the parameterless constructor, therfore;   
-// the number of columns is equal to the number of buttons we   
-// are adding to the frame. The row count remains one.  
-frameObj.add(btn1); frameObj.add(btn2); frameObj.add(btn3);  
+        // creating 9 buttons
+        ImageIcon img = new ImageIcon("./images/mic.png");
 
-  
-// setting the grid layout using the parameterless constructor    
-frameObj.setLayout(new GridLayout(1, 3, 20, 100));    
-  
-  
-frameObj.setSize(800, 600);    
-frameObj.setVisible(true);    
-}  
-  
-// main method  
-public static void main(String argvs[])   
-{    
-new GridLayoutExample();    
-}    
-}    
+        JButton btn1 = new JButton(img);
+        JButton btn2 = new JButton(img);
+        JButton btn3 = new JButton(img);
+
+        // adding buttons to the frame
+        // since, we are using the parameterless constructor, therfore;
+        // the number of columns is equal to the number of buttons we
+        // are adding to the frame. The row count remains one.
+        frameObj.add(btn1, BorderLayout.NORTH);
+        frameObj.add(btn2);
+        frameObj.add(btn3);
+
+        // setting the grid layout using the parameterless constructor
+        frameObj.setLayout(new GridLayout(1, 3, 20, 100));
+
+        frameObj.setSize(800, 600);
+        frameObj.setVisible(true);
+    }
+
+    // main method
+    public static void main(String argvs[]) {
+        new GridLayoutExample();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    
+    public static void main(String[] args)
+    {
+        Stage s = new Stage("./src/neo.csv");
+        s.print();
+    }
+}
